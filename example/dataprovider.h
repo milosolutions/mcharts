383420a25f170c253b3e8969431e386f6810cd0d
Tomasz Siekierda (sierdzio) <tsiekierda@milosolutions.com>
2017-08-17 08:03:24 +0200

    Update version to 1.0

--- example/dataprovider.h
+++ example/dataprovider.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
-Copyright (C) 2016 Milo Solutions
+Copyright (C) 2017 Milo Solutions
 Contact: https://www.milosolutions.com
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
